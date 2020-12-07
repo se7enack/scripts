94031d38cc31a35ec30b0a682884dcde0cb8f306
Stephen Burke <se7enack@users.noreply.github.com>
2020-12-07 18:50:41 -0500

    Update get_headers.py

--- get_headers.py
+++ get_headers.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+
+# Stephen Burke 2020-Dec-7
+
 import requests
 
-
 try:
-    url = str(input('Enter a url (please specify http or https):  ')).lower()
+    url = str(input('Enter a url (please specify with http or https):  ')).lower()
     response = requests.get(url)
     print("\n", url, "\n")
     print(response, "\n")
     print(response.headers, "\n")
 except:
     print("Error")
+   
