ad17efa5420ae72644980f6f37bb44fad33fac31
Stephen Burke <se7enack@users.noreply.github.com>
2020-12-07 23:33:53 -0500

    Update get_headers.py

--- get_headers.py
+++ get_headers.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python3
+#!/Users/user/miniconda/bin/python
 
 # Stephen Burke 2020-Dec-7
 
 import requests
+from re import search
 from pprint import pprint
 
-url = str(input('\nEnter a url (please specify with http or https):  ')).lower()
 
-try:
-	response = requests.get(url)
-	r = str(response.headers)
-	fixed = r.replace("\'", "\"")
-	print("\n", response, "\n")
-	pprint(fixed)
-	print("\n")
-except:
-    print("\nError: Well that's awkward\n")
+def tryit():
+	url = str(input('\nEnter a url (please specify with http or https):  ')).lower()
+	if search("http", url):
+		try:
+			response = requests.get(url)
+			r = str(response.headers)
+			fixed = r.replace("\'", "\"")
+			print("\n", response, "\n")
+			pprint(fixed)
+			print("\n")
+		except:
+		    print("\nError: Well that's awkward\n")
+	else:
+	    print("Please specify http:// or https://")
+	    tryit()
+tryit()
    
